5c0d9a62e1b56b649cec7a8ebdc8cf5f8ec07614
buffalosushi817 <44121873+buffalosushi817@users.noreply.github.com>
2021-03-31 00:25:28 -0700

    completed ex2

--- js/ex2.js
+++ js/ex2.js
@@ -1,124 +1,49 @@
 
-  window.addEventListener("load", myLoadEvent);
 
-  function myLoadEvent() {
-    const countryList   = [
-      "Afghanistan",
-      "Albania",
-      "Algeria",
-      "Andorra",
-      "Angola",
-      "Anguilla",
-      "Antarctica",
-      "Antigua-and-Barbuda",
-      "Argentina",
-      "Armenia",
-      "Aruba",
-      "Australia",
-      "Autria",
-      "Azerbaïjan"
-    ];
+function myFunction() {
 
-    document.getElementById("country").addEventListener("change", autocomplete(document.getElementById("country"), countryList ));
+  let myVal = document.getElementById("txtInput").value;
+  if (myVal !='') {
+    getData();
   }
-  
-
-  function autocomplete(inp, arr) {
-    /*the autocomplete function takes two arguments,
-    the text field element and an array of possible autocompleted values:*/
-    var currentFocus;
-    /*execute a function when someone writes in the text field:*/
-    inp.addEventListener("input", function(e) {
-        var a, b, i, val = this.value;
-        /*close any already open lists of autocompleted values*/
-        closeAllLists();
-        if (!val) { return false;}
-        currentFocus = -1;
-        /*create a DIV element that will contain the items (values):*/
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        /*append the DIV element as a child of the autocomplete container:*/
-        this.parentNode.appendChild(a);
-        /*for each item in the array...*/
-        for (i = 0; i < arr.length; i++) {
-          /*check if the item starts with the same letters as the text field value:*/
-          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-            /*create a DIV element for each matching element:*/
-            b = document.createElement("DIV");
-            /*make the matching letters bold:*/
-            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-            b.innerHTML += arr[i].substr(val.length);
-            /*insert a input field that will hold the current array item's value:*/
-            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-            /*execute a function when someone clicks on the item value (DIV element):*/
-                b.addEventListener("click", function(e) {
-                /*insert the value for the autocomplete text field:*/
-                inp.value = this.getElementsByTagName("input")[0].value;
-                /*close the list of autocompleted values,
-                (or any other open lists of autocompleted values:*/
-                closeAllLists();
-            });
-            a.appendChild(b);
-          }
-        }
-    });
-    /*execute a function presses a key on the keyboard:*/
-    inp.addEventListener("keydown", function(e) {
-        var x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        if (e.keyCode == 40) {
-          /*If the arrow DOWN key is pressed,
-          increase the currentFocus variable:*/
-          currentFocus++;
-          /*and and make the current item more visible:*/
-          addActive(x);
-        } else if (e.keyCode == 38) { //up
-          /*If the arrow UP key is pressed,
-          decrease the currentFocus variable:*/
-          currentFocus--;
-          /*and and make the current item more visible:*/
-          addActive(x);
-        } else if (e.keyCode == 13) {
-          /*If the ENTER key is pressed, prevent the form from being submitted,*/
-          e.preventDefault();
-          if (currentFocus > -1) {
-            /*and simulate a click on the "active" item:*/
-            if (x) x[currentFocus].click();
-          }
-        }
-    });
-    function addActive(x) {
-      /*a function to classify an item as "active":*/
-      if (!x) return false;
-      /*start by removing the "active" class on all items:*/
-      removeActive(x);
-      if (currentFocus >= x.length) currentFocus = 0;
-      if (currentFocus < 0) currentFocus = (x.length - 1);
-      /*add class "autocomplete-active":*/
-      x[currentFocus].classList.add("autocomplete-active");
-    }
-    function removeActive(x) {
-      /*a function to remove the "active" class from all autocomplete items:*/
-      for (var i = 0; i < x.length; i++) {
-        x[i].classList.remove("autocomplete-active");
-      }
-    }
-    function closeAllLists(elmnt) {
-      /*close all autocomplete lists in the document,
-      except the one passed as an argument:*/
-      var x = document.getElementsByClassName("autocomplete-items");
-      for (var i = 0; i < x.length; i++) {
-        if (elmnt != x[i] && elmnt != inp) {
-        x[i].parentNode.removeChild(x[i]);
-      }
-    }
+  else {
+    alert('Please enter a lookup name!')
+    document.getElementById("txtInput").focus();
   }
-  /*execute a function when someone clicks in the document:*/
-  document.addEventListener("click", function (e) {
-      closeAllLists(e.target);
-  });
-  } 
+
+}
+
+function getData() {
+  let myObj = []
+  const url = 'https://api.github.com/users/' + document.getElementById("txtInput").value;
+  let v_name = document.getElementById("lblName");
+  let v_blog = document.getElementById("lblBlog");
+  let v_create = document.getElementById("lblCreated");
+  let v_img = document.getElementById("img-github");
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => myObj.push({name: data.name, blog: data.blog, created_at: data.created_at,avatar_url: data.avatar_url }))
+  .then(function () {
+      v_name.innerHTML = myObj[0].name;
+      v_blog.innerHTML = myObj[0].blog;
+      v_create.innerHTML = myObj[0].created_at;
+      v_img.src = myObj[0].avatar_url;
+      v_img.src = myObj[0].avatar_url;
+      v_img.style.display ='block';
+  }
+  )
+  .catch(function (err) {
+    console.log('Fetch Error: ', err);
+  })
+}
 
 
-  
+function clearValues() {
+  document.getElementById("lblName").innerHTML ='';
+  document.getElementById("lblBlog").innerHTML ='';
+  document.getElementById("lblCreated").innerHTML ='';
+  document.getElementById("img-github").src ='';
+  document.getElementById("img-github").style.display ='none';
+  document.getElementById("txtInput").value ='';
+}
