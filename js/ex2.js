fb9b4ab66f56a1f432c0a1c1faa2eb604a4e631f
buffalosushi817 <44121873+buffalosushi817@users.noreply.github.com>
2021-03-31 23:49:03 -0700

    started on ex4.

--- js/ex2.js
+++ js/ex2.js
@@ -1,7 +1,34 @@
 
 
-function myFunction() {
+window.addEventListener("load", bind);
 
+function bind() {
+  document.getElementById("results").style.display='none';
+  
+  var input = document.getElementById("btnLookup");
+  input.addEventListener("keyup", function(event) {
+    if (event.key === 13) {
+      event.preventDefault();
+      document.getElementById("btnLookup").click();
+    }
+  }); 
+
+}
+
+
+function showResults() {
+  var x = document.getElementById("results");
+    x.style.display = "block";
+} 
+
+function hideResults() {
+  var x = document.getElementById("results");
+    x.style.display = "none";
+} 
+
+
+
+function myFunction() {
   let myVal = document.getElementById("txtInput").value;
   if (myVal !='') {
     getData();
@@ -25,15 +52,24 @@ function getData() {
   .then(response => response.json())
   .then(data => myObj.push({name: data.name, blog: data.blog, created_at: data.created_at,avatar_url: data.avatar_url }))
   .then(function () {
+    if (myObj[0].name != undefined) {
       v_name.innerHTML = myObj[0].name;
       v_blog.innerHTML = myObj[0].blog;
       v_create.innerHTML = myObj[0].created_at;
       v_img.src = myObj[0].avatar_url;
       v_img.src = myObj[0].avatar_url;
       v_img.style.display ='block';
+      showResults();
+    }
+    else {
+      clearValues();
+      alert('No records found!')
+    }
+
   }
   )
   .catch(function (err) {
+    hideResults();
     console.log('Fetch Error: ', err);
   })
 }
@@ -46,4 +82,6 @@ function clearValues() {
   document.getElementById("img-github").src ='';
   document.getElementById("img-github").style.display ='none';
   document.getElementById("txtInput").value ='';
+  document.getElementById("txtInput").focus();
+  hideResults();
 }
--- js/ex4.js
+++ js/ex4.js
@@ -1,62 +1,21 @@
 
 
-window.addEventListener("load", myLoadEvent);
+window.addEventListener("load", bind);
 
-function myLoadEvent() {
-  const houses = [
-    {
-      code: "ST",
-      name: "Stark"
-    },
-    {
-      code: "LA",
-      name: "Lannister"
-    },
-    {
-      code: "BA",
-      name: "Baratheon"
-    },
-    {
-      code: "TA",
-      name: "Targaryen"
-    }
-  ];
-  
-  let x = document.getElementById("house");
-  for (let i = 0; i < houses.length; i++) { 
-    let option = document.createElement("option");
-    option.text = houses[i].name;
-    option.value = houses[i].code;
-    x.add(option);
-  }
-
-  document.getElementById("house").addEventListener("change", myFunction);
+function bind() {
+  document.getElementById("btnSubmit").addEventListener("click", showResults);
+  document.getElementById("btnCancel").addEventListener("click", hideResults);
+  document.getElementById("results").style.display='none';
 }
 
-function myFunction() {
-  document.getElementById("characters").innerHTML = "";
-  const getCharacters = houseCode => {
-    switch (houseCode) {
-      case "ST":
-        return ["Eddard", "Catelyn", "Robb", "Sansa", "Arya", "Jon Snow"];
-      case "LA":
-        return ["Tywin", "Cersei", "Jaime", "Tyrion"];
-      case "BA":
-        return ["Robert", "Stannis", "Renly"];
-      case "TA":
-        return ["Aerys", "Daenerys", "Viserys"];
-      default:
-        return []; // Empty array
-    }
-  };
 
-  let e = document.getElementById("house");   
-  let ul = document.getElementById("characters");
-  
-  for (let i = 0; i < getCharacters(e.value).length; i++) {
-      let name = getCharacters(e.value)[i];
-      let li = document.createElement('li');
-      li.appendChild(document.createTextNode(name));
-      ul.appendChild(li);
-  }
-}
+
+function showResults() {
+  var x = document.getElementById("results");
+    x.style.display = "block";
+} 
+
+function hideResults() {
+  var x = document.getElementById("results");
+    x.style.display = "none";
+} 
