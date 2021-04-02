5e01db1fc13b654c80c6e48bf6ede0b257b1e640
buffalosushi817 <44121873+buffalosushi817@users.noreply.github.com>
2021-04-01 21:19:10 -0700

    completed ex4

--- js/ex4.js
+++ js/ex4.js
@@ -11,11 +11,68 @@ function bind() {
 
 
 function showResults() {
-  var x = document.getElementById("results");
-    x.style.display = "block";
+    if (document.getElementById("txtName").value==='' ) {
+      alert('Please enter a name!');
+      document.getElementById("txtName").focus();
+    }
+
+    else if (document.getElementById("txtEmail").value==='') {
+      alert('Please enter an email address!')
+      document.getElementById("txtEmail").focus();
+    }
+
+    else if (emailIsValid(document.getElementById("txtEmail").value)==false) {
+      alert('Please enter a valid email address!')
+      document.getElementById("txtEmail").focus();
+    }
+
+    else if (document.getElementById("cboLocation").value ==='') {
+      alert('Please select a preferred location!')
+    }
+    else {
+      getData();
+    }
+    
 } 
+
+function emailIsValid (email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}
 
 function hideResults() {
   var x = document.getElementById("results");
     x.style.display = "none";
+
+    document.getElementById("txtName").value='';
+    document.getElementById("txtEmail").value='';
+    document.getElementsByName("rdoPayType").checked = false;
+    document.getElementById("chkPromotion").checked = false;
+    document.getElementById("cboLocation").value ='';
+
 } 
+
+function getData() {
+
+  let v_name = document.getElementById("txtName").value;
+  let v_email = document.getElementById("txtEmail").value;
+  let v_payType = '';
+
+  var rdo = document.getElementsByName('rdoPayType');     
+  for(i = 0; i < rdo.length; i++) {
+      if(rdo[i].checked)
+      v_payType = rdo[i].value;
+  }
+
+  let v_promotion =document.getElementById("chkPromotion").checked; 
+  let v_location = document.getElementById("cboLocation").value;
+
+  document.getElementById("lblName").innerHTML = v_name;
+  document.getElementById("lblEmail").innerHTML = v_email;
+  document.getElementById("lblPayType").innerHTML = v_payType;
+  document.getElementById("lblPromotion").innerHTML = v_promotion;
+  document.getElementById("lblLocation").innerHTML = v_location;
+
+  var x = document.getElementById("results");
+  x.style.display = "block";
+
+}
